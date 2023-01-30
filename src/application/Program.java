43cfd2da35604ef42da6b6f46c790bdb08fbe587
Allisson Bezerra <allisson.geografia@gmail.com>
2023-01-30 17:58:57 -0300

    Adicionando quaquer data

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,8 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.Date;
 
 import model.entities.Department;
@@ -7,12 +10,21 @@ import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
+		/* Passando qualquer data 
+			LocalDate localDate = LocalDate.parse("2023-01-30");
+			Date date = new SimpleDateFormat("yyyy-MM-dd").parse(localDate.toString());
 		
+			// ou assim  
+		
+		
+		LocalDate localDate = LocalDate.parse("2021-01-15");
+		Date date = new Date(localDate.atStartOfDay(ZoneId.of("America/Sao_Paulo")).toEpochSecond() * 1000);
+			*/		
 		Department obj = new Department(1, "Books");
 		
-				
-		Seller seller = new Seller( 21, "Allisson", "ama@jjFla", new Date(), 4000.00, obj);
+		Seller seller = new Seller( 21, "Allisson", "ama@jjFla", new Date(), 4000.00, obj); // usando new Date() pega a data do dia
+ 		
 		System.out.println(seller);
 	}
 
