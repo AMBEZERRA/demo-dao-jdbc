91df62c5ff21ee1a455b2e43facf121e272c986c
Allisson Bezerra <allisson.geografia@gmail.com>
2023-02-01 09:58:34 -0300

    Delete department competed

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,6 +1,8 @@
 package application;
 
 
+import java.util.Scanner;
+
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
@@ -8,15 +10,27 @@ import model.entities.Department;
 public class Program2 {
 
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
 		DepartmentDao newdepartmentDao = DaoFactory.createDepartmentDao();
 		
-		
+		/*
 		System.out.println("=== Test number one ISERT INTO FROM Department ");
 		Department department = new Department(null, "Music");
 		newdepartmentDao.insert(department);
 		System.out.println("ID department Insert " + department.getId());	
-	
+		*/
+		
+		System.out.println("=== Test number two DELETE FROM Department ");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		newdepartmentDao.deleteBy(id);
+		System.out.println("Delete completed !!!");
+		
+		
+		
+		
+		
+		sc.close();
 	}
 
 }
